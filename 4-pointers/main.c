94c47c9691a5766e73c12c240f237e8afcefabd4
CitruzAsake <Citruz@hotmail.dk>
2021-02-12 15:22:29 +0100

    Foreslaesning 4

---
+++ 4-pointers/main.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<limits.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void set0(double x){ x=0; }
+
+void set0p(double *x){ (*x)=0; }
+
+void print_array(int n, double* a){
+	for(int i=0;i<n;i++)printf("print_array: a[%d]=%g\n",i,i[a]);
+int i=0;while(i<n){
+	printf("print_array: a[%d]=%g\n",i,i[a]);
+	i++;
+	}
+i=0;do{
+	printf("print_array: a[%d]=%g\n",i,i[a]);
+	i++;
+	}while(i<n-1);
+}
+
+int main(){
+	double y=1;
+	set0(y);
+	printf("y after set0  = %g\n",y);
+	set0p(&y);
+	printf("y after set0p = %g\n",y);
+	int n=5;
+	double v[n];
+	for(int i=0;i<n;i++){ // i++ : i=i+1
+		v[i]=i;
+	}
+	print_array(n,v);
+	int N=7;
+	double* a = malloc(N*sizeof(double));
+	for(int i=0;i<N;i++) a[i]=i+100;
+	print_array(N,a);
+
+free(a);	
+return 0;
+}
