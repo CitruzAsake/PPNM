ce29bf7b9c057aa40fa16f37bfb73c23eef4e4c4
CitruzAsake <Citruz@hotmail.dk>
2021-06-23 10:57:00 +0200

    Interpolation B done

---
+++ Homeworks/interpolation/B/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_interp.h>
+#include <assert.h>
+
+typedef struct {
+   int n;      
+   double* x; 
+   double* y;
+   double* b;
+   double* c;
+} qspline;
+
+qspline* qspline_alloc(int n, double* x, double* y);
+double qspline_integ(qspline *s, double input);
+double qspline_eval(qspline *s, double input);
+double qspline_deriv(qspline* s, double x_new);
+void qspline_free(qspline *s);
+
+int main() 
+{
+    int n=30;
+	double x[n],y[n];
+
+	for(int i=0;i<n;i++)
+    {
+		x[i]=i;
+		y[i]=sin(i*i);
+	}
+
+	printf("# index 0: Data points\n");
+	for(int i=0;i<n;i++) printf("%g %g\n",x[i],y[i]);
+	printf("\n\n");
+
+    printf("# index 1: Interpolations\n");
+    qspline* s = qspline_alloc(n, x, y);
+        
+	double dz=0.1;
+	for(double z=x[0];z<=x[n-1];z+=dz){
+		double qz=qspline_eval(s,z);
+		printf("%g %g\n",z,qz);       
+    }     
+	printf("\n\n");
+    printf("# index 2: Integral\n");
+	for(double z=x[0];z<=x[n-1];z+=dz){
+		double integ=qspline_integ(s,z);
+		printf("%g %g\n",z,integ);
+	}
+	printf("\n\n");
+    printf("# index 2: Derivative\n"); 
+	for(double z=x[0];z<=x[n-1];z+=dz){
+		double deriv=qspline_deriv(s,z);
+		printf("%g %g\n",z,deriv);
+    }
+
+    qspline_free(s);
+return 0;
+}
---
+++ Homeworks/interpolation/B/quadraturespline.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <assert.h>
+#include<stdio.h>
+#include<math.h>
+#include<gsl/gsl_vector.h>
+
+typedef struct {int n; double* x, *y, *b, *c;} qspline;
+
+double qspline_eval(qspline *s, double input){
+        int i = binsearch_array(s->n,s->x,input);
+        double dx=input-s->x[i];
+        double output=s->y[i]+dx*(s->b[i]+dx*s->c[i]);
+return output;
+}
+
+double qspline_integ(qspline *s, double input)
+{
+        int i = binsearch_array(s->n,s->x,input);
+        double output = 0;
+        double dx=0;
+        for (int p=1;p<=i;p++){
+                dx=s->x[p]-s->x[p-1];
+                output+=dx*(s->y[p-1]+dx*(s->b[p-1]/2+dx*s->c[p-1]/3));
+        }
+        dx=input-s->x[i];
+        output+=dx*(s->y[i]+dx*(s->b[i]/2+dx*s->c[i]/3));
+
+return output;
+}
+
+
+qspline* qspline_alloc(int n, double* x, double* y)
+{
+    qspline *s = (qspline*)malloc(sizeof(qspline));
+	s->b = (double*)malloc((n-1)*sizeof(double));
+	s->c = (double*)malloc((n-1)*sizeof(double));
+	s->x = (double*)malloc(n*sizeof(double));
+	s->y = (double*)malloc(n*sizeof(double));
+	s->n = n;
+
+	int i=0;
+	for(i=0;i<n;i++){
+		s->x[i]=x[i];
+		s->y[i]=y[i];
+	}
+	double dy[n-1], dx[n-1];
+	for (i=0;i<n-1;i++){
+		dx[i]=x[i+1]-x[i];
+		dy[i]=(y[i+1]-y[i])/dx[i];
+	}
+	s->c[0]=0;
+	for (i=0;i<n-2;i++){
+		s->c[i+1]=(dy[i+1]-dy[i]-s->c[i]*dx[i])/dx[i+1];
+	}
+	s->c[n-2]/=2;
+	for (i=n-3;i>=0;i--){
+		s->c[i]=(dy[i+1]-dy[i]-s->c[i+1]*dx[i+1])/dx[i];
+	}
+	for (i=0;i<n-1;i++){
+		s->b[i]=dy[i]-s->c[i]*dx[i];
+
+	}
+return s;
+}
+
+double qspline_deriv(qspline* s, double x_new)
+{
+   int i = binsearch_array(s->n, s->x, x_new);
+
+   double deriv = s->b[i] + 2*(s->c[i])*(x_new - s->x[i]);
+   return deriv;
+}
+
+void qspline_free(qspline *s)
+{
+	free(s->x);
+	free(s->y);
+	free(s->b);
+	free(s->c);
+	free(s);
+}
