c62d4c84ca358d2b014bddc6fb83a59380f10948
CitruzAsake <Citruz@hotmail.dk>
2021-06-24 08:23:44 +0200

    8 Homework A done

---
+++ Homeworks/Root_Finding/NewtonJacobi.c
@@ -0,0 +1,78 @@
+//In this file we have the same functions as the "ordinary" jacobi file but in addition we also have the NEWTONS METHOD JACOBI
+
+#include<stdio.h>
+#include<math.h>
+#include<assert.h>
+#include<gsl/gsl_vector.h>
+#include<gsl/gsl_matrix.h>
+#include<gsl/gsl_blas.h>
+#ifndef DBL_EPSILON
+#define DBL_EPSILON 2.22045e-16
+#endif
+double DELTA=sqrt(DBL_EPSILON);
+int LIMIT=10000;
+#include<float.h>
+
+
+// pulled functions. 
+
+void GS_decomp(gsl_matrix* A, gsl_matrix* R);
+
+void GS_solve(gsl_matrix* Q, gsl_matrix* R, gsl_vector* b, gsl_vector* x);
+
+void GS_inverse(gsl_matrix* Q, gsl_matrix* R, gsl_matrix* B);
+
+double dot(gsl_vector* x, gsl_vector* y);
+
+double norm(gsl_vector* x);
+
+//Now for NEWTONS METHOD JACOBI
+
+int Newton(void f(gsl_vector* x, gsl_vector* fx), gsl_vector* x, double eps)
+{
+	int n=x->size, steps=0;
+	gsl_matrix* J= gsl_matrix_alloc(n,n);
+	gsl_matrix* R= gsl_matrix_alloc(n,n);
+	gsl_vector* fx= gsl_vector_alloc(n);
+	gsl_vector* z= gsl_vector_alloc(n);
+	gsl_vector* fz= gsl_vector_alloc(n);
+	gsl_vector* df= gsl_vector_alloc(n);
+	gsl_vector* Dx= gsl_vector_alloc(n);
+	while(steps<LIMIT)
+	{
+	f(x,fx);
+	for (int j=0; j<n;j++){ 
+		double xj= gsl_vector_get(x,j);
+		gsl_vector_set(x,j,xj+DELTA);
+		f(x,df);
+		gsl_vector_sub(df,fx);
+		for(int i =0;i<n; i++)
+			gsl_matrix_set(J,i,j,gsl_vector_get(df,i)/DELTA);
+		gsl_vector_set(x,j,xj);
+		}
+	GS_decomp(J,R);
+	GS_solve(J,R,fx,Dx);
+	gsl_vector_scale(Dx,-1);
+	double s=1;
+	while(s>1./64)
+		{
+			gsl_vector_memcpy(z,x);
+			gsl_vector_add(z,Dx);
+			f(z,fz);
+			if(gsl_blas_dnrm2(fz)<(1-s/2)*gsl_blas_dnrm2(fx)) break;
+			s*=0.5;
+			gsl_vector_scale(Dx,0.5);
+		}
+	gsl_vector_memcpy(x,z);
+	gsl_vector_memcpy(fx,fz);
+	if( gsl_blas_dnrm2(Dx)<DELTA || gsl_blas_dnrm2(fx)<eps) break;
+	steps++; 
+	}
+	gsl_matrix_free(J);
+	gsl_vector_free(fx);
+	gsl_vector_free(z);
+	gsl_vector_free(fz);
+	gsl_vector_free(df);
+	gsl_vector_free(Dx);
+return steps;
+}
---
+++ Homeworks/Root_Finding/main.c
@@ -0,0 +1,41 @@
+#include<gsl/gsl_matrix.h>
+#include<gsl/gsl_vector.h>
+#include<stdio.h>
+#include<float.h>
+#include<math.h>
+
+
+static int ncalls;
+
+void Jacobian(void f(gsl_vector* x,gsl_vector* fx), gsl_vector* x);
+
+int Newton(void f(gsl_vector* x,gsl_vector* fx), gsl_vector* x, double eps);
+
+void vector_print(gsl_vector* vec);
+
+void Rosenbrock(gsl_vector* x, gsl_vector* fx){
+    double X = gsl_vector_get(x,0);
+    double Y = gsl_vector_get(x,1);
+    gsl_vector_set(fx,0,2*(1-X)-4*X*100*(Y-X*X));
+    gsl_vector_set(fx,1,2*100*(Y-X*X));
+}
+
+void f(gsl_vector* p,gsl_vector* fx){
+	ncalls++;
+	double x=gsl_vector_get(p,0), y=gsl_vector_get(p,1);
+	gsl_vector_set(fx,0, 2*(1-x)*(-1)+100*2*(y-x*x)*(-1)*2*x);
+	gsl_vector_set(fx,1, 100*2*(y-x*x));
+	}
+
+int main()
+{
+    gsl_vector* x = gsl_vector_alloc(2);
+    gsl_vector_set(x,0,1.3);
+    gsl_vector_set(x,1,1.3);
+    double eps = 1e-5;
+    Newton(Rosenbrock,x,eps);
+    printf("-----------------NEWTON solved problem A-----------------------\n");
+    printf("The extremum Rosenbrock valley function has location (x= %g,y=%g) \n", gsl_vector_get(x,0),gsl_vector_get(x,1));
+    printf("---------------------------------------------------------------");
+return 0;    
+}
